b48cc730863542e17f12c2be377ad394858bae1d
Ariel Carrera <carreraariel@gmail.com>
2019-07-23 01:42:21 -0300

    Test without Agroal

--- src/test/java/com/github/arielcarrera/cdi/test/config/JtaEnvironment.java
+++ src/test/java/com/github/arielcarrera/cdi/test/config/JtaEnvironment.java
@@ -1,25 +1,45 @@
 package com.github.arielcarrera.cdi.test.config;
 
+import javax.naming.InitialContext;
+
+import org.h2.jdbcx.JdbcDataSource;
 import org.jnp.server.NamingBeanImpl;
 import org.junit.rules.ExternalResource;
 
+import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
 import com.arjuna.ats.jta.utils.JNDIManager;
+import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
 
 public class JtaEnvironment extends ExternalResource {
 
-    private NamingBeanImpl NAMING_BEAN;
+	private NamingBeanImpl NAMING_BEAN;
 
-    @Override
-    protected void before() throws Throwable {
-        NAMING_BEAN = new NamingBeanImpl();
-        NAMING_BEAN.start();
+	@Override
+	protected void before() throws Throwable {
+		NAMING_BEAN = new NamingBeanImpl();
+		// Start JNDI server
+		NAMING_BEAN.start();
+		// Bind JTA implementation with default names
+		JNDIManager.bindJTAImplementation();
 
-        JNDIManager.bindJTAImplementation();
-        
-    }
+		// Bind datasource
+		TransactionalConnectionProvider.bindDataSource();
+		// Set transaction log location
+		setObjectStoreDir();
+	}
 
-    @Override
-    protected void after() {
-        NAMING_BEAN.stop();
-    }
+	@Override
+	protected void after() {
+		NAMING_BEAN.stop();
+	}
+
+
+	static void setObjectStoreDir() {
+		BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).setObjectStoreDir("target/tx-object-store");
+		BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore")
+				.setObjectStoreDir("target/tx-object-store");
+		BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "stateStore")
+				.setObjectStoreDir("target/tx-object-store");
+	}
+
 }
---
+++ src/test/java/com/github/arielcarrera/cdi/test/config/TransactionalConnectionProvider.java
@@ -0,0 +1,79 @@
+package com.github.arielcarrera.cdi.test.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.service.UnknownUnwrapTypeException;
+
+import com.arjuna.ats.jdbc.TransactionalDriver;
+
+public class TransactionalConnectionProvider implements ConnectionProvider {
+
+    public static final String DATASOURCE_JNDI = "java:testDS";
+
+    public static final String USERNAME = "sa";
+
+    public static final String PASSWORD = "";
+
+    private final TransactionalDriver transactionalDriver;
+
+    public TransactionalConnectionProvider() {
+        transactionalDriver = new TransactionalDriver();
+    }
+
+    public static void bindDataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        dataSource.setUser(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        try {
+            InitialContext initialContext = new InitialContext();
+            initialContext.bind(DATASOURCE_JNDI, dataSource);
+        }
+        catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    public Connection getConnection() throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty(TransactionalDriver.userName, USERNAME);
+        properties.setProperty(TransactionalDriver.password, PASSWORD);
+        return transactionalDriver.connect("jdbc:arjuna:" + DATASOURCE_JNDI, properties);
+    }
+
+    @Override
+    public void closeConnection(Connection connection) throws SQLException {
+        if (!connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    @Override
+    public boolean supportsAggressiveRelease() {
+        return false;
+    }
+
+    @Override
+    public boolean isUnwrappableAs(Class aClass) {
+        return getClass().isAssignableFrom(aClass);
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> aClass) {
+        if (isUnwrappableAs(aClass)) {
+            return (T) this;
+        }
+
+        throw new UnknownUnwrapTypeException(aClass);
+    }
+
+}
